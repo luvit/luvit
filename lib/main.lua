1b6738f8aa4a9b339910701368bce36923023dcd
Tim Caswell <tim@creationix.com>
2011-09-20 15:44:25 -0700

    Implement enough to get parsing events to printf statements
    Change-Id: Ieabd25aefb17567bde9b259b5a8d0b5a83562be3

--- lib/main.lua
+++ lib/main.lua
@@ -1,10 +1,43 @@
-print("Using uv version " .. uv.VERSION_MAJOR .. "." .. uv.VERSION_MINOR)
-print(uv)
+print("uv", uv, "version " .. uv.VERSION_MAJOR .. "." .. uv.VERSION_MINOR)
+print("http_parser", http_parser, "version " .. http_parser.VERSION_MAJOR .. "." .. http_parser.VERSION_MINOR)
 
-print("Using http_parser version " .. http_parser.VERSION_MAJOR .. "." .. http_parser.VERSION_MINOR)
-print(http_parser)
+local request = ([[
+GET /documentation/apache/ HTTP/1.0
+Connection: Keep-Alive
+User-Agent: Mozilla/4.01 [en] (Win95; I)
+Host: hal.etc.com.au
+Accept: image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*
+Accept-Language: en
+Accept-Charset: iso-8859-1,*,utf-8
+
+]]):gsub("\n","\r\n")
+
+
+local parser = http_parser.new("request", {
+  on_message_begin = function () end,
+  on_url = function (url) end,
+  on_header_field = function (field) end,
+  on_header_value = function (value) end,
+  on_headers_complete = function (info) end,
+  on_body = function (chunk) end,
+  on_message_complete = function () end
+})
+--parser:reinitialize("request")
+local nparsed = parser:execute(request, 0, #request)
+print("executed " .. nparsed .. " bytes")
+--parser:finish()
+
+
+--local parser = http_parser.new("request", {})
+--print(parser)
+--print(parser:execute("Hello", 4, 1))
+--parser.execute()
 --print(http_parser.new())
 --print(http_parser.new("Hello"))
 --print(http_parser.new("request"))
-print(http_parser.new("request", {}))
+--print(http_parser.new("REQUEST", {}))
+--print(http_parser.new("request", {}))
+
+
+--local HTTPParser = require('http_parser')
 
--- src/lhttp_parser.c
+++ src/lhttp_parser.c
@@ -1,35 +1,174 @@
 #include "lhttp_parser.h"
 #include "http_parser.h"
+#include <string.h>
 
+
+static struct http_parser_settings lhttp_parser_settings;
+
+static const char* method_to_str(unsigned short m) {
+  switch (m) {
+    case HTTP_DELETE:     return "DELETE";
+    case HTTP_GET:        return "GET";
+    case HTTP_HEAD:       return "HEAD";
+    case HTTP_POST:       return "POST";
+    case HTTP_PUT:        return "PUT";
+    case HTTP_CONNECT:    return "CONNECT";
+    case HTTP_OPTIONS:    return "OPTIONS";
+    case HTTP_TRACE:      return "TRACE";
+    case HTTP_COPY:       return "COPY";
+    case HTTP_LOCK:       return "LOCK";
+    case HTTP_MKCOL:      return "MKCOL";
+    case HTTP_MOVE:       return "MOVE";
+    case HTTP_PROPFIND:   return "PROPFIND";
+    case HTTP_PROPPATCH:  return "PROPPATCH";
+    case HTTP_UNLOCK:     return "UNLOCK";
+    case HTTP_REPORT:     return "REPORT";
+    case HTTP_MKACTIVITY: return "MKACTIVITY";
+    case HTTP_CHECKOUT:   return "CHECKOUT";
+    case HTTP_MERGE:      return "MERGE";
+    case HTTP_MSEARCH:    return "MSEARCH";
+    case HTTP_NOTIFY:     return "NOTIFY";
+    case HTTP_SUBSCRIBE:  return "SUBSCRIBE";
+    case HTTP_UNSUBSCRIBE:return "UNSUBSCRIBE";
+    default:              return "UNKNOWN_METHOD";
+  }
+}
+
+static int lhttp_parser_on_message_begin(http_parser *p) {
+  printf("on_message_begin\n");
+  return 0;
+}
+
+static int lhttp_parser_on_url(http_parser *p, const char *at, size_t length) {
+  printf("on_url %.*s\n", length, at);
+  return 0;
+}
+
+static int lhttp_parser_on_header_field(http_parser *p, const char *at, size_t length) {
+  printf("on_header_field %.*s\n", length, at);
+  return 0;
+}
+
+static int lhttp_parser_on_header_value(http_parser *p, const char *at, size_t length) {
+  printf("on_header_value %.*s\n", length, at);
+  return 0;
+}
+
+static int lhttp_parser_on_headers_complete(http_parser *p) {
+
+  printf("on_headers_complete\n");
+
+  // METHOD
+  if (p->type == HTTP_REQUEST) {
+    printf("\tmethod %s\n", method_to_str(p->method));
+  }
+
+  // STATUS
+  if (p->type == HTTP_RESPONSE) {
+    printf("\tstatus_code %d\n", p->status_code);
+  }
+
+  // VERSION
+  printf("\tversion_major %d\n", p->http_major);
+  printf("\tversion_minor %d\n", p->http_minor);
+
+  printf("\tshould_keep_alive %s\n", http_should_keep_alive(p) ? "true": "false");
+
+  printf("\tupgrade %s\n", p->upgrade ? "true": "false");
+
+  return 0;
+}
+
+static int lhttp_parser_on_body(http_parser *p, const char *at, size_t length) {
+  printf("on_body %.*s\n", length, at);
+  return 0;
+}
+
+static int lhttp_parser_on_message_complete(http_parser *p) {
+  printf("on_message_complete\n");
+  return 0;
+}
 
 // Takes as arguments a string for type and a table for event callbacks
-static int http_parser_new (lua_State *L) {
+static int lhttp_parser_new (lua_State *L) {
 
   const char *type = luaL_checkstring(L, 1);
-  luaL_argcheck(L, (0 == strcasecmp(type, "request")) || (0 == strcasecmp(type, "response")), 1, "type must be 'request' or 'response'");
   luaL_checktype(L, 2, LUA_TTABLE);
 
-  // TODO: make userdata instance and return it instead of this string
-  lua_pushstring(L, type);
+  http_parser* parser = (http_parser*)lua_newuserdata(L, sizeof(http_parser));
+
+  if (0 == strcmp(type, "request")) {
+    http_parser_init(parser, HTTP_REQUEST);
+  } else if (0 == strcmp(type, "response")) {
+    http_parser_init(parser, HTTP_RESPONSE);
+  } else {
+    return luaL_argerror(L, 1, "type must be 'request' or 'response'");
+  }
+  
+  // TODO: find out what value would make sense for data since we're not in C++
+  // parser->data = this?
+
+  luaL_getmetatable(L, "lhttp_parser");
+  lua_setmetatable(L, -2);
+
   return 1;
 }
 
-static const luaL_reg http_parserlib[] = {
-  {"new", http_parser_new},
+// execute(parser, buffer, offset, length)
+static int lhttp_parser_execute (lua_State *L) {
+  http_parser* parser = (http_parser *)luaL_checkudata(L, 1, "lhttp_parser");
+
+  luaL_checktype(L, 2, LUA_TSTRING);
+  size_t chunk_len;
+  const char *chunk = lua_tolstring(L, 2, &chunk_len);
+  
+  size_t offset = luaL_checkint(L, 3);
+  size_t length = luaL_checkint(L, 4);
+  
+  luaL_argcheck(L, offset < chunk_len, 3, "Offset is out of bounds");
+  luaL_argcheck(L, offset + length <= chunk_len, 4,  "Length extends beyond end of chunk");
+
+  size_t nparsed = http_parser_execute(parser, &lhttp_parser_settings, chunk + offset, length);
+
+  lua_pushnumber(L, nparsed);
+  return 1;
+}
+
+static const struct luaL_Reg lhttp_parser_f [] = {
+  {"new", lhttp_parser_new},
   {NULL, NULL}
 };
 
-/*
-** Open uv library
-*/
-LUALIB_API int luaopen_http_parser (lua_State *L) {
-  luaL_register(L, "http_parser", http_parserlib);
+static const struct luaL_Reg lhttp_parser_m [] = {
+  {"execute", lhttp_parser_execute},
+  {NULL, NULL}
+};
 
+LUALIB_API int luaopen_http_parser (lua_State *L) {
+
+  // This needs to be done sometime?
+  lhttp_parser_settings.on_message_begin    = lhttp_parser_on_message_begin;
+  lhttp_parser_settings.on_url              = lhttp_parser_on_url;
+  lhttp_parser_settings.on_header_field     = lhttp_parser_on_header_field;
+  lhttp_parser_settings.on_header_value     = lhttp_parser_on_header_value;
+  lhttp_parser_settings.on_headers_complete = lhttp_parser_on_headers_complete;
+  lhttp_parser_settings.on_body             = lhttp_parser_on_body;
+  lhttp_parser_settings.on_message_complete = lhttp_parser_on_message_complete;
+
+  // Set up our Lua module
+  luaL_newmetatable(L, "lhttp_parser");
+  /* metatable.__index = metatable */
+  lua_pushvalue(L, -1); /* duplicates the metatable */
+  lua_setfield(L, -2, "__index");
+  luaL_register(L, NULL, lhttp_parser_m);
+  luaL_register(L, "http_parser", lhttp_parser_f);
+
+  // Stick version info on the http_parser table
   lua_pushnumber(L, HTTP_PARSER_VERSION_MAJOR);
   lua_setfield(L, -2, "VERSION_MAJOR");
   lua_pushnumber(L, HTTP_PARSER_VERSION_MINOR);
   lua_setfield(L, -2, "VERSION_MINOR");
-  
+
   return 1;
 }
 
