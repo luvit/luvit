ee7c89a22d616adb8d81b7b7c4ba43d40b6caa08
Tim Caswell <tim@creationix.com>
2011-09-23 16:54:55 -0700

    Fix bug so stack works again

--- src/luv.c
+++ src/luv.c
@@ -1,32 +1,43 @@
 #include "luv.h"
 #include "uv.h"
 #include <stdlib.h>
+#include <assert.h>
 
-// Stores value at index in the environment of current userdata (index 1) at given name
+// Registers a callback
 static void luv_register_event(lua_State* L, const char* name, int index) {
-  printf("Registering %s from index %d\n", name, index);
+  int before = lua_gettop(L);
+  luaL_checkudata(L, 1, "luv_tcp");
   lua_getfenv(L, 1);
+  if (lua_isfunction (L, index) == 0) {
+    printf("ERROR, missing function\n");
+  }
   lua_pushvalue(L, index);
   lua_setfield(L, -2, name);
   lua_pop(L, 1);
+  assert(lua_gettop(L) == before);
 }
 
 // Emit an event of the current userdata consuming nargs
+// Assumes userdata is right below args
 static void luv_emit_event(lua_State* L, const char* name, int nargs) {
-  printf("Emitting event %s, %d\n", name, nargs);
+  int before = lua_gettop(L);
   // Load the connection callback
-  lua_getfenv(L, 1);
+  lua_getfenv(L, -nargs - 1);
   lua_getfield(L, -1, name);
   lua_remove(L, -2);
   if (lua_isfunction (L, -1) == 0) {
     printf("missing event: on_%s\n", name);
     lua_pop(L, 1 + nargs);
+    assert(lua_gettop(L) == before - nargs);
     return;
   }
+
+  // move the function below the args
   lua_insert(L, -nargs - 1);
-  if (lua_pcall(L, nargs, 1, 0) != 0) {
+  if (lua_pcall(L, nargs, 0, 0) != 0) {
     error(L, "error running function 'on_%s': %s", name, lua_tostring(L, -1));
   }
+  assert(lua_gettop(L) == before - nargs);
 }
 
 static int luv_run (lua_State* L) {
