fec8d339a8eca212ee490961322fa1af82841010
Tim Caswell <tim@creationix.com>
2011-09-22 22:52:21 -0700

    Clean up main.lua

--- lib/main.lua
+++ lib/main.lua
@@ -1,14 +1,5 @@
-local dump = require('lib/utils').dump
-function p(...)
-  local l = ""
-  for i, v in ipairs{...} do
-    if (i > 1) then
-      l = l .. "\t" .. dump(v)
-    else
-      l = l .. dump(v)
-    end
-  end
-  print(l)
+function p(o)
+  print(require('lib/utils').dump(o))
 end
 
 
