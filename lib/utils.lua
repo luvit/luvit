c9ff30eac9302e849421ac408fe0db12e9762169
Tim Caswell <tim@creationix.com>
2011-09-23 09:17:34 -0700

    Make colors be names so it can be used externally

--- lib/utils.lua
+++ lib/utils.lua
@@ -1,23 +1,27 @@
-local black   = "0;30"
-local red     = "0;31"
-local green   = "0;32"
-local yellow  = "0;33"
-local blue    = "0;34"
-local magenta = "0;35"
-local cyan    = "0;36"
-local white   = "0;37"
-local Bblack   = "1;30"
-local Bred     = "1;31"
-local Bgreen   = "1;32"
-local Byellow  = "1;33"
-local Bblue    = "1;34"
-local Bmagenta = "1;35"
-local Bcyan    = "1;36"
-local Bwhite   = "1;37"
+local colors = {
+  black   = "0;30",
+  red     = "0;31",
+  green   = "0;32",
+  yellow  = "0;33",
+  blue    = "0;34",
+  magenta = "0;35",
+  cyan    = "0;36",
+  white   = "0;37",
+  Bblack   = "1;30",
+  Bred     = "1;31",
+  Bgreen   = "1;32",
+  Byellow  = "1;33",
+  Bblue    = "1;34",
+  Bmagenta = "1;35",
+  Bcyan    = "1;36",
+  Bwhite   = "1;37"
+}
 
-function color(string, color, old)
-  if (old) then
-    return "\27[" .. color .. "m" .. string .. "\27[" .. old .. "m"
+function color(string, color_name, reset_name)
+  local color = colors[color_name] or "0"
+  if (reset_name) then
+    local reset_color = colors[reset_name]
+    return "\27[" .. color .. "m" .. string .. "\27[" .. reset_color .. "m"
   else
     return "\27[" .. color .. "m" .. string .. "\27[0m"
   end
@@ -29,20 +33,20 @@ function dump(o, depth)
   end
   local indent = ("  "):rep(depth)
   if type(o) == 'nil' then
-    return color("nil", Bblack)
+    return color("nil", "Bblack")
   end
   if type(o) == 'boolean' then
-    return color(tostring(o), yellow)
+    return color(tostring(o), "yellow")
   end
   if type(o) == 'number' then
-    return color(tostring(o), blue)
+    return color(tostring(o), "blue")
   end
   if type(o) == 'string' then
-    return color('"', Bgreen, green) .. o:gsub("\n",color("\\n",Bgreen, green)):gsub("\r",color("\\r",Bgreen, green)):gsub("\t",color("\\t",Bgreen, green)) .. color('"', Bgreen)
+    return color('"', "Bgreen", "green") .. o:gsub("\n",color("\\n", "Bgreen", "green")):gsub("\r",color("\\r", "Bgreen", "green")):gsub("\t",color("\\t", "Bgreen", "green")) .. color('"', "Bgreen")
   end
   if type(o) == 'table' then
     if (depth > 1) then
-      return color(tostring(o), yellow)
+      return color(tostring(o), "yellow")
     end
 
     -- Check to see if this is an array
@@ -68,7 +72,7 @@ function dump(o, depth)
         if type(k) == "string" and k:find("^[%a_][%a%d_]*$") then
           s = s .. k .. ' = '
         else
-          s = s .. color('[', white) .. dump(k, 100) ..color(']', white) .. ' = '
+          s = s .. color('[', "white") .. dump(k, 100) ..color(']', "white") .. ' = '
         end
       end
       s = s .. dump(v, depth + 1)
@@ -80,10 +84,10 @@ function dump(o, depth)
     end
   end
   if type(o) == 'userdata' then
-    return color(tostring(o), red)
+    return color(tostring(o), "red")
   end
   if type(o) == 'function' then
-    return color(tostring(o), cyan)
+    return color(tostring(o), "cyan")
   end
   -- This doesn't happen right?
   return tostring(o)
