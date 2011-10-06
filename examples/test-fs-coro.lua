5343d5859e90210b73e3009f386d1f5c77d6f22d
Tim Caswell <tim@creationix.com>
2011-10-05 18:04:17 -0700

    Add more debugging to the coro example

--- examples/test-fs-coro.lua
+++ examples/test-fs-coro.lua
@@ -1,24 +1,32 @@
 local uv = require('uv');
 
-local co = coroutine.create(function (filename) 
-  
+local co
+co = coroutine.create(function (filename)
+
+  print("opening...")
   uv.fs_open(filename, 'r', "0644", resume)
+  p("yielding", co, coroutine.status(co))
   local fd = coroutine.yield()
   p("on_open", {fd=fd})
 
+  print("reading...")
   uv.fs_read(fd, 0, 4096, resume)
+  p("yielding", co, coroutine.status(co))
   local chunk, length = coroutine.yield()
   p("on_read", {chunk=chunk, length=length})
-  
+
+  print("closing...")
   uv.fs_close(fd, resume)
+  p("yielding", co, coroutine.status(co))
   coroutine.yield()
   p("on_close")
 
 end)
 
 function resume(...)
-  p("Resuming...", co, ...)
-  p(coroutine.resume(co, ...))
+  p("Resuming1...", co, coroutine.status(co), ...)
+  p("result", coroutine.resume(co, ...))
+  p("After...", co, coroutine.status(co), ...)
 end
 
 resume("license.txt")
