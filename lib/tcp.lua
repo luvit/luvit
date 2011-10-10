eaa79525c217b3ca6b912dbfc87692d512ad2d88
Tim Caswell <tim@creationix.com>
2011-10-10 15:32:08 -0700

    Add create_server wrapper for tcp servers
    Change-Id: I0452aeb9fd8ec3f1d1511859ca14289f1ab7384b

--- lib/tcp.lua
+++ lib/tcp.lua
@@ -1,11 +1,12 @@
 local UV = require('uv')
 local user_meta = require('utils').user_meta
 local stream_meta = require('stream').meta
+local TCP = {}
 
 local tcp_prototype = {}
 setmetatable(tcp_prototype, stream_meta)
 
-local function new_tcp()
+function TCP.new()
   local tcp = {
     userdata = UV.new_tcp(),
     prototype = tcp_prototype
@@ -14,8 +15,21 @@ local function new_tcp()
   return tcp
 end
 
-return {
-  new = new_tcp,
-  prototype = tcp_prototype,
-  meta = tcp_meta
-}
+function TCP.create_server(ip, port, on_connection)
+  local server = TCP.new()
+  server:bind("0.0.0.0", 8080)
+
+  server:listen(function (err, status)
+    if (err) then
+      return server:emit("error", err)
+    end
+    local client = TCP.new()
+    server:accept(client)
+    client:read_start()
+    on_connection(client)
+  end)
+
+  return server
+end
+
+return TCP
