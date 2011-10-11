32b98ff69a12c902d986ba116f4cdaa636115979
Tim Caswell <tim@creationix.com>
2011-10-10 23:55:08 -0700

    Fix type on http server example

--- examples/http-server.lua
+++ examples/http-server.lua
@@ -2,7 +2,7 @@ local HTTP = require("http")
 local Utils = require("utils")
 
 HTTP.create_server("0.0.0.0", 8080, function (req, res)
-  local body = Utils.dump({req=req,headers=headers}) .. "\n"
+  local body = Utils.dump({req=req,headers=req.headers}) .. "\n"
   res:write_head(200, {
     ["Content-Type"] = "text/plain",
     ["Content-Length"] = #body
