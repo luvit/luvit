4a19abba645bf24ff5400eefd1c3bd237c225572
Tim Caswell <tim@creationix.com>
2011-10-12 11:35:42 -0700

    Clean up coroutines
    Change-Id: Ia4d673e77959b3e4aaebeb16179682ebad34aaeb

--- examples/fs-coroutines.lua
+++ examples/fs-coroutines.lua
@@ -1,47 +1,40 @@
 local FS = require('fs');
 local Fiber = require('fiber')
-local open = Fiber.wrap(FS.open, 3)
-local fstat = Fiber.wrap(FS.fstat, 1)
-local read = Fiber.wrap(FS.read, 3)
-local close = Fiber.wrap(FS.close, 1)
-local readdir = Fiber.wrap(FS.readdir, 1)
 
-
-Fiber.new(function (co)
+Fiber.new(function (resume, wait)
 
   print("opening...")
-  local err, fd = open(co, "license.txt", "r", "0644")
-  p("on_open", {err=err, fd=fd})
-  if (err) then return end
+  FS.open("license.txt", "r", "0644", resume)
+  local err, fd = wait()
+  p("on_open", {err=err,fd=fd})
 
   print("fstatting...")
-  local err, stat = fstat(co, fd)
-  p("stat", {err=err, stat=stat})
-  if (err) then return end
+  FS.fstat(fd, resume)
+  local err, stat = wait()
+  p("stat", {err=err,stat=stat})
 
   print("reading...")
   local offset = 0
   repeat
-    local err, chunk = read(co, fd, offset, 128)
-    local length = #chunk
-    p("on_read", {err=err, chunk=chunk, offset=offset, length=length})
-    if (err) then return end
+    FS.read(fd, offset, 72, resume)
+    local err, chunk, length = wait()
+    p("on_read", {err=err,chunk=chunk, offset=offset, length=length})
     offset = offset + length
   until length == 0
 
   print("closing...")
-  local err = close(co, fd)
+  FS.close(fd, resume)
+  local err = wait()
   p("on_close", {err=err})
-  if (err) then return end
 
 end)
 
-Fiber.new(function (co)
+Fiber.new(function (resume, wait)
 
   print("scanning directory...")
-  local err, files = readdir(co, ".")
-  p("on_open", {err=err, files=files})
-  if (err) then return end
+  FS.readdir(".", resume)
+  local err, files = wait()
+  p("on_open", {err=err,files=files})
 
 end)
 
--- lib/fiber.lua
+++ lib/fiber.lua
@@ -2,31 +2,10 @@ local Coroutine = require('coroutine')
 local Fiber = {}
 
 function Fiber.new(fn)
-  local co = Coroutine.create(fn)
-  assert(Coroutine.resume(co, co))
+  local resume = Coroutine.wrap(fn)
+  resume(resume, Coroutine.yield)
 end
 
--- Convert a callback based function to work with coroutines
-function Fiber.wrap(fn, nargs)
-  return function (coro, ...)
-    local function resume(...)
-      assert(Coroutine.resume(coro, ...))
-    end
-    local args = {...}
-    if nargs == 1 then
-      fn(args[1], resume)
-    elseif nargs == 2 then
-      fn(args[1], args[2], resume)
-    elseif nargs == 3 then
-      fn(args[1], args[2], args[3], resume)
-    elseif nargs == 4 then
-      fn(args[1], args[2], args[3], args[4], resume)
-    else
-      error("Too many nargs")
-    end
-    return Coroutine.yield()
-  end
-end
 
 return Fiber
 
