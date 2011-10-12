42491e1abebafa90a5ebc16e0e9a511d408b5471
Tim Caswell <tim@creationix.com>
2011-10-12 16:04:18 -0700

    Add asserts to luv_acall for sanity checking
    Change-Id: I767e8bce9d072fb430116d4cd80d54d74b8e2e17

--- src/utils.c
+++ src/utils.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "utils.h"
 
 // Meant as a lua_call replace for use in async callbacks
 // Uses the main loop and event source
 void luv_acall(lua_State *C, int nargs, int nresults, const char* source) {
+  int beforeC = lua_gettop(C);
 
   // Get the main thread without cheating
   lua_getfield(C, LUA_REGISTRYINDEX, "main_thread");
   lua_State* L = lua_tothread(C, -1);
+  int beforeL = lua_gettop(L);
   lua_pop(C, 1);
 
   // If C is not main then move to main
@@ -18,19 +21,21 @@ void luv_acall(lua_State *C, int nargs, int nresults, const char* source) {
     lua_pushstring(L, source);
     lua_xmove (C, L, nargs + 1);
     lua_call(L, nargs + 2, nresults);
-    return;
-  }
+    assert(lua_gettop(L) == beforeL);
+  } else {
 
-  // Wrap the call with the event_source function
-  int offset = nargs + 2;
-  lua_getglobal(L, "event_source");
-  lua_insert(L, -offset);
-  lua_pushstring(L, source);
-  lua_insert(L, -offset);
+    // Wrap the call with the event_source function
+    int offset = nargs + 2;
+    lua_getglobal(L, "event_source");
+    lua_insert(L, -offset);
+    lua_pushstring(L, source);
+    lua_insert(L, -offset);
 
-  if (lua_pcall(L, nargs + 2, nresults, 0)) {
-    luaL_error(L, "EVERR '%s': %s", source, lua_tostring(L, -1));
+    if (lua_pcall(L, nargs + 2, nresults, 0)) {
+      luaL_error(L, "EVERR '%s': %s", source, lua_tostring(L, -1));
+    }
   }
+  assert(lua_gettop(C) == beforeC - nargs - 1);
 
 }
 
