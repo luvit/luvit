a51ebe57516bf4196408a18ec30c12286b7a3778
Tim Caswell <tim@creationix.com>
2011-10-15 23:58:36 -0700

    Add start to SDL example

---
+++ examples/sdl/boxes.lua
@@ -0,0 +1,120 @@
+local FS = require('fs')
+local Timer = require('timer')
+local FFI = require('ffi')
+local Debug = require('debug')
+local Math = require('math')
+local SDL = require('sdl')
+local Bit = require('bit')
+
+p("SDL", SDL)
+local Rect = FFI.metatype("SDL_Rect", {})
+
+SDL.Init(Bit.bor(SDL.INIT_VIDEO,SDL.INIT_JOYSTICK))
+SDL.WM_SetCaption("Luvit!", nil);
+local screen = SDL.SetVideoMode(1024, 768, 0, SDL.RESIZABLE)
+
+local joysticks = {}
+SDL.JoystickEventState(SDL.ENABLE);
+p("numjoysticks", SDL.NumJoysticks())
+for i = 1,SDL.NumJoysticks() do
+  p("Found ", FFI.string(SDL.JoystickName(i-1)));
+  local joy = SDL.JoystickOpen(i-1)
+  joysticks[i] = joy
+end
+p("Joysticks", joysticks)
+
+local function exit()
+  SDL.stop_events()
+  SDL.Quit();
+  print("Thanks for Playing!");
+end
+
+SDL:on(SDL.QUIT, exit)
+
+SDL:on(SDL.KEYDOWN, function (evt)
+  p("on_keydown", evt)
+  local sym = evt.key.keysym.sym
+  if sym == SDL.ffi.SDLK_ESCAPE then
+    exit()
+  end
+end)
+
+local x = screen.w / 2
+local y = screen.h / 2
+local mx = 0
+local my = 0
+
+SDL:on(SDL.JOYAXISMOTION, function (evt)
+  local j = evt.jaxis
+  p("JOYAXISMOTION", {which=j.which,axis=j.axis,value=j.value})
+  -- +0 right -0 left
+  -- +1 down  -1 up
+  if j.axis == 0 then
+    mx = j.value / 0x8000
+  elseif j.axis == 1 then
+    my = j.value / 0x8000
+  end
+end)
+
+SDL:on(SDL.JOYHATMOTION, function (evt)
+  local j = evt.jhat
+  p("JOYHATMOTION", {which=j.which,hat=j.hat,value=j.value})
+end)
+
+SDL:on(SDL.JOYBUTTONDOWN, function (evt)
+  local j = evt.jbutton
+p("SDL", SDL)
+
+  p("JOYBUTTONDOWN", {which=j.which,button=j.button,state=j.state})
+end)
+
+SDL:on(SDL.JOYBUTTONUP, function (evt)
+  local j = evt.jbutton
+  p("JOYBUTTONUP", {which=j.which,button=j.button,state=j.state})
+end)
+
+local fade_color = SDL.MapRGBA(screen.format, 0, 0, 0, 1)
+
+
+local spin = 2
+local grow = 1.02
+
+local w = FFI.new("int[1]", screen.w)
+local h = FFI.new("int[1]", screen.h)
+SDL.GFX.rotozoomSurfaceSize(screen.w, screen.h, spin, grow, w, h)
+w = w[0]
+h = h[0]
+
+SDL:on(SDL.VIDEORESIZE, function (evt)
+  p("ON_VIDEORESIZE", {w=evt.resize.w,h=evt.resize.h})
+  local old = screen
+  screen = SDL.SetVideoMode(evt.resize.w, evt.resize.h, 0, SDL.RESIZABLE)
+  SDL.FreeSurface(old)
+  local w = FFI.new("int[1]", screen.w)
+  local h = FFI.new("int[1]", screen.h)
+  SDL.GFX.rotozoomSurfaceSize(screen.w, screen.h, spin, grow, w, h)
+  w = w[0]
+  h = h[0]
+
+end)
+
+SDL:on('tick', function (delta)
+  
+  x = x + mx * delta
+  y = y + my * delta
+
+
+  
+  local tmp = SDL.GFX.rotozoomSurface(screen, spin, grow, 1)
+  SDL.ffi.SDL_UpperBlit(tmp, Rect((w - screen.w)/2,(h - screen.h)/2,screen.w,screen.h), screen, null)
+  SDL.ffi.SDL_FreeSurface(tmp)
+  local color = Math.random(0x100000000)
+  SDL.GFX.filledCircleColor(screen, x, y, 50, color)
+  
+  -- Flush the output
+  SDL.Flip(screen)
+
+end)
+
+p("SDL", SDL)
+
---
+++ examples/sdl/sdl.lua
@@ -0,0 +1,98 @@
+local FFI = require('ffi')
+local Emitter = require('emitter')
+local Timer = require('timer')
+local UV = require('uv')
+
+-- Use blocking IO since this is startup stuff
+FFI.cdef(require('io').open('ffi_SDL.h', 'r'):read('*a'))
+
+local SDL = {
+}
+
+local lazy = {
+  ffi = function () return FFI.load('SDL') end,
+  GFX = function () return FFI.load("SDL_gfx") end,
+  TTF = function () return FFI.load("SDL_ttf") end,
+  Mix = function () return FFI.load("SDL_mixer") end,
+
+  INIT_AUDIO       = 0x00000010,
+  INIT_VIDEO       = 0x00000020,
+  INIT_CDROM       = 0x00000100,
+  INIT_JOYSTICK    = 0x00000200,
+  INIT_NOPARACHUTE = 0x00100000, --< Don't catch fatal signals
+  INIT_EVENTTHREAD = 0x01000000, --< Not supported on all OS's
+  INIT_EVERYTHING  = 0x0000FFFF,
+  
+  ANYFORMAT        = 0x10000000, --< Allow any video depth/pixel-format
+  HWPALETTE        = 0x20000000, --< Surface has exclusive palette
+  DOUBLEBUF        = 0x40000000, --< Set up double-buffered video mode
+  FULLSCREEN       = 0x80000000, --< Surface is a full screen display
+  OPENGL           = 0x00000002, --< Create an OpenGL rendering context
+  OPENGLBLIT       = 0x0000000A, --< Create an OpenGL rendering context and use it for blitting
+  RESIZABLE        = 0x00000010, --< This video mode may be resized
+  NOFRAME          = 0x00000020, --< No window caption or edge frame
+
+  QUERY            = -1,
+  IGNORE           = 0,
+  DISABLE          = 0,
+  ENABLE           = 1,
+}
+
+local events = false
+
+setmetatable(SDL, {
+  __index = function (table, key)
+    if key == 'userdata' or key == "handlers" then return end
+    if lazy[key] then
+      local value = lazy[key]
+      if type(value) == "function" then
+        value = value()
+      end
+      rawset(table, key, value)
+      return value
+    end
+    if Emitter.prototype[key] then
+      if not events then
+        init_events()
+      end
+      local value = Emitter.prototype[key]
+      rawset(table, key, value)
+      return value
+    end
+    return table.ffi["SDL_" .. key]
+  end
+})
+
+function init_events()
+  events = true
+  local event = FFI.new("SDL_Event")
+  local alive = true
+  local before = UV.now()
+  local interval = Timer.set_interval(1, function ()
+    local now = UV.now()
+    local delta = now - before
+    before = now
+
+    while SDL.PollEvent(event) > 0 do
+      if alive then
+        SDL:emit("event", event)
+        SDL:emit(event.type, event)
+      end
+    end
+    if alive then
+      SDL:emit('tick', delta)
+    end
+  end)
+
+  function SDL.stop_events()
+    Timer.clear_timer(interval)
+    alive = false
+  end
+
+end
+
+
+return SDL
+
+
+
