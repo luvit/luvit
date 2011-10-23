381cc8c8dfa2744f01c93039f9493bbc0fabf8ca
Tim Caswell <tim@creationix.com>
2011-10-23 02:06:07 -0700

    Add sample stack app

---
+++ examples/app/server.lua
@@ -0,0 +1,16 @@
+local HTTP = require "http"
+local Stack = require "stack"
+
+local MethodEcho = require "method_echo"
+local UrlEcho = require "url_echo"
+
+HTTP.create_server("0.0.0.0", 8080, Stack.stack(
+  Stack.mount("/methods",
+    MethodEcho("PUT"),
+    MethodEcho("GET")
+  ),
+  Stack.mount("/echo", UrlEcho),
+  MethodEcho("DELETE")
+))
+
+print("Server listening at http://localhost:8080/")
