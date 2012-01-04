f9e2c70c8f3dbc95a54babb9dd84e74b8d29448c
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-04 13:57:03 -0600

    cleanup

--- src/luvit.c
+++ src/luvit.c
@@ -51,12 +51,13 @@ static int luvit_getcwd(lua_State* L) {
 int main(int argc, char *argv[])
 {
   int index, rc;
-  lua_State *L = luaL_newstate();
+  lua_State *L;
 
   rc = ares_library_init(ARES_LIB_INIT_ALL);
   assert(rc == ARES_SUCCESS);
   luv_dns_open();
 
+  L = luaL_newstate();
   if (L == NULL) {
     fprintf(stderr, "luaL_newstate has failed\n");
     return 1;
