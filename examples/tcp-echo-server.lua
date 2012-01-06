b5e6d4d886f6eff0f61ff51243168da78c89a705
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-05 22:48:47 -0600

    change tcp example to use net

--- examples/tcp-echo-server.lua
+++ examples/tcp-echo-server.lua
@@ -1,35 +1,36 @@
-local TCP = require('tcp')
+local net = require('net')
 
-local server = TCP.create_server("0.0.0.0", 8080, function (client)
+local server = net.createServer(function (client)
   p("on_connection", client)
 
   print("Adding listener for data events")
   client:on("data", function (chunk)
     p("on_read", chunk)
-    
+
     print("Sending chunk back to client")
     client:write(chunk, function (err)
       p("on_written", err)
     end)
 
   end)
-  
+
   print("Adding listener for close event")
   client:on("end", function ()
     p("on_end")
-    
+
     print("Closing connection")
     client:close(function ()
       p("on_closed")
     end)
   end)
-  
+
+end)
+
+server:listen(8080, '0.0.0.0', function(err)
+  print("TCP echo server listening on port 8080")
 end)
 
 print("Listening for errors in the server")
 server:on("error", function (err)
   p("ERROR", err)
 end)
-
-print("TCP echo server listening on port 8080")
-
