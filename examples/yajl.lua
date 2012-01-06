63db14fec96be9195d75b25fff3726cab8b4c18a
Tim Caswell <tim@creationix.com>
2012-01-06 16:07:53 -0600

    Making progress on yajl bindings

---
+++ examples/yajl.lua
@@ -0,0 +1,39 @@
+local Yajl = require('yajl')
+
+local parser = Yajl.new({
+  allow_comments = false,
+  on_null = function ()
+    p("on_null")
+  end,
+  on_boolean = function (value)
+    p("on_boolean", value)
+  end,
+  on_number = function (value)
+    p("on_number", value)
+  end,
+  on_string = function (value)
+    p("on_string", value)
+  end,
+  on_start_map = function ()
+    p("on_start_map")
+  end,
+  on_map_key = function (key)
+    p("on_map_key", key)
+  end,
+  on_end_map = function ()
+    p("on_end_map")
+  end,
+  on_start_array = function ()
+    p("on_start_array")
+  end,
+  on_end_array = function ()
+    p("on_end_array")
+  end,
+  on_error = function (err)
+    error(err)
+  end
+})
+
+parser.write("{")
+parser.write("[1,2,3]")
+parser.write("}")
--- src/lyajl.c
+++ src/lyajl.c
@@ -1,17 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "lyajl.h"
 #include "yajl/yajl_parse.h"
 #include "yajl/yajl_version.h"
 
-static const luaL_reg lyajl_f[] = {
-  {NULL, NULL}
+static int lyajl_on_null (void * ctx) {
+  return 1;
+}
+static int lyajl_on_boolean (void * ctx, int value) {
+  return 1;
+}
+static int lyajl_on_integer (void * ctx, long long value) {
+  return 1;
+}
+static int lyajl_on_double (void * ctx, double value) {
+  return 1;
+}
+static int lyajl_on_string (void * ctx, const unsigned char* value, size_t len) {
+  return 1;
+}
+static int lyajl_on_start_map (void * ctx) {
+  return 1;
+}
+static int lyajl_on_map_key (void * ctx, const unsigned char* key, size_t len) {
+  return 1;
+}
+static int lyajl_on_end_map (void * ctx) {
+  return 1;
+}
+static int lyajl_on_start_array (void * ctx) {
+  return 1;
+}
+static int lyajl_on_end_array (void * ctx) {
+  return 1;
+}
+
+static yajl_callbacks lyajl_callbacks = {
+    lyajl_on_null, lyajl_on_boolean,
+    lyajl_on_integer, lyajl_on_double, NULL,
+    lyajl_on_string,
+    lyajl_on_start_map, lyajl_on_map_key, lyajl_on_end_map,
+    lyajl_on_start_array, lyajl_on_end_array
 };
+
+static int lyajl_write (lua_State *L) {
+  luaL_error(L, "TODO: Implement lyajl_write");
+  return 0;
+}
+
+static int lyajl_new (lua_State *L) {
+  luaL_checktype(L, 1, LUA_TTABLE);
+  void* ctx;
+  yajl_handle handle = yajl_alloc(&lyajl_callbacks, NULL, ctx);
+
+  lua_newtable (L);
+  lua_pushlightuserdata(L, handle);
+  lua_setfield(L, -2, "handle");
+  lua_pushcfunction(L, lyajl_write);
+  lua_setfield(L, -2, "write");
+  return 1;
+}
 
 LUALIB_API int luaopen_yajl (lua_State *L) {
   // Create a new exports table
   lua_newtable (L);
-  // Put our functions on it
-  luaL_register(L, NULL, lyajl_f);
-  // Stick on version info
+
+  // With a single function
+  lua_pushcfunction(L, lyajl_new);
+  lua_setfield(L, -2, "new");
+
+  // And version info
   lua_pushnumber(L, YAJL_MAJOR);
   lua_setfield(L, -2, "VERSION_MAJOR");
   lua_pushnumber(L, YAJL_MINOR);
