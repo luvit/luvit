d580ca2f4b42c393715da85ac2e2523a73ceb1b6
Tim Caswell <tim@creationix.com>
2012-02-08 17:05:50 -0600

    Update the other examples

--- examples/uvffi/stub.c
+++
@@ -1,5 +0,0 @@
-#include "uv.h"
-#include "http_parser.h"
-// Convert this to a single header with:
-// gcc -I /home/tim/luvit/deps/uv/include -I /home/tim/luvit/deps/http-parser -E stub.c | grep -v '^#' > ffi_uv.h
-
