22bcbb04d07889af950554e80859b981ef96fdab
Ryan Phillips <ryan.phillips@rackspace.com>
2012-02-22 12:42:07 -0600

    generate MSVS 2010 project and use msbuild

--- tools/win_build.bat
+++ tools/win_build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-devenv.com /build Debug luvit.sln /project luvit.vcxproj
+msbuild /nologo /property:Configuration=Debug /target:win32 luvit.sln
