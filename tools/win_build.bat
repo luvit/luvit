b88899410ae87cf09876e5b4627aca1bae34b9de
Ryan Phillips <ryan.phillips@rackspace.com>
2012-02-22 12:44:24 -0600

    use luvit target

--- tools/win_build.bat
+++ tools/win_build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-msbuild /nologo /property:Configuration=Debug /target:win32 luvit.sln
+msbuild /nologo /property:Configuration=Debug /target:luvit luvit.sln
