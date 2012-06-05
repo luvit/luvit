e778ac927db23bb0c6c4114d96277fdba9127814
Tomaz Muraus <tomaz@tomaz.me>
2012-06-05 13:03:51 -0700

    Add tests for url.parse.

---
+++ tests/test-url.lua
@@ -0,0 +1,15 @@
+require('helper')
+
+local url = require('url')
+
+local path = '/somepath?test=bar&ponies=foo'
+
+local parsed = url.parse(path)
+print(parsed.pathname)
+print(parsed.query)
+assert(parsed.pathname == '/somepath')
+assert(parsed.query == 'test=bar&ponies=foo')
+
+local parsed = url.parse(path, true)
+assert(parsed.pathname == '/somepath')
+assert(deep_equal(parsed.query, {test = 'bar', ponies = 'foo'}))
