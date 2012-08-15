26fb985305f52715c8a1ea3e3a0546fc140592bf
Hiroaki Nakamura <hnakamur@gmail.com>
2012-08-15 23:42:21 +0900

    Port test-fs-mkdir from node.js

---
+++ tests/test-fs-mkdir.lua
@@ -0,0 +1,84 @@
+--[[
+
+Copyright 2012 The Luvit Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http:--www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
+require("helper")
+
+local FS = require('fs')
+local Path = require('path')
+
+function unlink(pathname)
+  pcall(FS.rmdirSync, pathname)
+end
+
+local tmpDir = __dirname .. '/tmp'
+
+(function()
+  local ncalls = 0
+  local pathname = tmpDir .. '/test1'
+
+  unlink(pathname)
+
+-- TODO
+--  FS.mkdir(pathname, function(err)
+  FS.mkdir(pathname, 511 --[[tonumber('0777',8]], function(err)
+    assert(err == null)
+    assert(FS.existsSync(pathname) == true)
+    ncalls = ncalls + 1
+  end)
+
+  process:on('exit', function()
+    unlink(pathname)
+    assert(ncalls == 1)
+  end)
+end)();
+
+(function()
+  local ncalls = 0
+  local pathname = tmpDir .. '/test2'
+
+  unlink(pathname)
+
+  FS.mkdir(pathname, 511 --[[tonumber('0777',8]], function(err)
+    assert(err == null)
+    assert(FS.existsSync(pathname) == true)
+    ncalls = ncalls + 1
+  end)
+
+  process:on('exit', function()
+    unlink(pathname)
+    assert(ncalls == 1)
+  end)
+end)();
+
+(function()
+  local pathname = tmpDir .. '/test3'
+
+  unlink(pathname)
+-- TODO
+--  FS.mkdirSync(pathname)
+  FS.mkdirSync(pathname, '0777')
+
+  local exists = FS.existsSync(pathname)
+  unlink(pathname)
+
+  assert(exists == true)
+end)()
+
+-- Keep the event loop alive so the async mkdir() requests
+-- have a chance to run (since they don't ref the event loop).
+process.nextTick(function() end)
