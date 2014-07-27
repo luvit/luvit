b6f018c60a9a67df979da9b4666264e36eb6f82c
Ryan Phillips <ryan.phillips@rackspace.com>
2014-07-27 18:54:18 -0500

    Revert "Merge pull request #264 from tim-smart/feature/querystring-stringify"
    This reverts commit fcf4479ba975b5ed7daa62eaeaf93059e5eba9c4, reversing
    changes made to eeb845a0b2dc59662868140f14c02eadf90f9c13.

--- lib/luvit/querystring.lua
+++ lib/luvit/querystring.lua
@@ -20,7 +20,6 @@ limitations under the License.
 -- querystring helpers
 local querystring = {}
 
-local table = require('table')
 local string = require('string')
 local find = string.find
 local gsub = string.gsub
@@ -38,7 +37,6 @@ function querystring.urldecode(str)
   str = gsub(str, '\r\n', '\n')
   return str
 end
-querystring.urldecodecomponent = querystring.urldecode
 
 function querystring.urlencode(str)
   if str then
@@ -47,16 +45,6 @@ function querystring.urlencode(str)
       return format('%%%02X', byte(c))
     end)
     str = gsub(str, ' ', '+')
-  end
-  return str
-end
-
-function querystring.urlencodecomponent(str)
-  if str then
-    str = gsub(str, '\n', '\r\n')
-    str = gsub(str, '([^%w])', function(c)
-      return format('%%%02X', byte(c))
-    end)
   end
   return str
 end
@@ -77,83 +65,6 @@ function querystring.parse(str, sep, eq)
     end
   end
   return vars
-end
-
--- Make sure value is converted to a valid string representation
--- for querystring use
-function toquerystring(val, vtype)
-  vtype = vtype or type(val)
-
-  if 'table' == vtype then
-    return ''
-  elseif 'string' == vtype then
-    return val
-  end
-
-  return tostring(val)
-end
-
--- Insert a item into a querystring result table
-function insertqueryitem(ret, key, val, sep, eq)
-  local vtype = nil -- string
-  local skey = nil -- string (Safe key)
-  local count = 0
-
-  vtype = type(val)
-  skey = querystring.urlencodecomponent(key, sep, eq)
-
-  -- only use numeric keys for table values
-  if 'table' == vtype then
-    for i, v in ipairs(val) do
-      if nil ~= val then
-        count = count + 1
-        v = querystring.urlencodecomponent(toquerystring(v), sep, eq)
-        table.insert(ret, table.concat({skey, v}, eq))
-      end
-    end
-
-    if 0 == count then
-      table.insert(ret, table.concat({skey, ''}, eq))
-    end
-
-    count = 0
-  else
-    val = querystring.urlencodecomponent(toquerystring(val, vtype), sep, eq)
-    table.insert(ret, table.concat({skey, val}, eq))
-  end
-end
-
--- Create a querystring from the given table.
-function querystring.stringify(params, order, sep, eq)
-  if not params then
-    return ''
-  end
-
-  order = order or nil
-  sep = sep or '&'
-  eq = eq or '='
-  local ret = {}
-  local vtype = nil -- string
-  local count = 0
-  local skey = nil -- string
-
-  if order then
-    local val = nil -- mixed
-    for i, key in ipairs(order) do
-      val = params[key]
-      insertqueryitem(ret, key, val, sep, eq)
-    end
-  else
-    for key, val in pairs(params) do
-      insertqueryitem(ret, key, val, sep, eq)
-    end
-  end
-
-  if 0 == #ret then
-    return ''
-  end
-
-  return table.concat(ret, sep)
 end
 
 -- module
