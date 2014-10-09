28643232a94201620f4510121722906bc03864ac
Ryan Phillips <ryan.phillips@rackspace.com>
2014-10-09 13:54:40 -0500

    fix(fs_call): memory leak

--- src/luv_fs.c
+++ src/luv_fs.c
@@ -225,6 +225,8 @@ uv_fs_t* luv_fs_store_callback(lua_State* L, int index) {
     }                                                                         \
     argc = luv_process_fs_result(L, req);                                 \
     lua_remove(L, -argc - 1);                                                 \
+    free(req->data);                                                          \
+    uv_fs_req_cleanup(req);                                                   \
     return argc;                                                              \
   } while (0)
 
