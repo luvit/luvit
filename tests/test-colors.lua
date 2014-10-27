3c029c283d9328938e3a9f8989891a78bd3988f7
Tim Caswell <tim@creationix.com>
2014-10-27 16:49:42 -0500

    Make colors test automated

--- tests/test-colors.lua
+++ tests/test-colors.lua
@@ -1,54 +1,57 @@
-local ffi = require('ffi')
 local utils = require('utils')
 local dump = utils.dump
-local p = utils.prettyPrint
+local strip = utils.strip
 
-utils.loadColors(256)
-print("256 utils")
-p(utils)
+require('tap')(function (test)
 
-utils.loadColors(16)
-print("16 utils")
-p(utils)
+  test("Recursive values", function ()
+    local data = {a="value"}
+    data.data = data
+    local out = dump(data)
+    local stripped = strip(out)
+    print("recursive", out, dump(stripped))
+    assert(string.match(stripped, "{ a = 'value', data = table: 0x%x+ }"))
+  end)
 
-utils.loadColors()
-print("no-color")
-p(utils)
-p(dump(utils))
+  test("string escapes", function ()
+    local tests = {
+      '\000\001\002\003\004\005\006\a\b\t\n\v\f\r\014\015',  "'\\000\\001\\002\\003\\004\\005\\006\\a\\b\\t\\n\\v\\f\\r\\014\\015'",
+      '\016\017\018\019\020\021\022\023\024\025\026\027\028\029\030\031',  "'\\016\\017\\018\\019\\020\\021\\022\\023\\024\\025\\026\\027\\028\\029\\030\\031'",
+      ' !"#$%&\'()*+,-./', '\' !"#$%&\\\'()*+,-./\'',
+      '0123456789:;<=>?',  "'0123456789:;<=>?'",
+      '@ABCDEFGHIJKLMNO',  "'@ABCDEFGHIJKLMNO'",
+      'PQRSTUVWXYZ[\\]^_', "'PQRSTUVWXYZ[\\\\]^_'",
+      '`abcdefghijklmno',  "'`abcdefghijklmno'",
+      'pqrstuvwxyz{|}',  "'pqrstuvwxyz{|}'",
+    }
+    for i = 1, 16, 2 do
+      local out = dump(tests[i])
+      local stripped = strip(out)
+      print(out, dump(stripped))
+      assert(stripped == tests[i + 1])
+    end
 
-utils.loadColors(256)
+  end)
 
-p{"This is \n\r\t cool \0 right?"}
-p{1, 2, true, false, nil, "Hello"}
+  test("Color mode switching", function ()
+    local data = {42,true,"A\nstring"}
 
-local str = ""
-for i = 0, 127 do
-  str = str .. string.char(i)
-end
+    utils.loadColors()
+    local plain = dump(data)
+    print("plain", plain, dump(plain))
+    assert(plain == "{ 42, true, 'A\\nstring' }")
 
-p(str)
+    utils.loadColors(16)
+    local colored = dump(data)
+    print("colored", colored, dump(colored))
+    assert(colored == "\027[1;30m{ \027[0m\027[1;33m42\027[0m\027[1;30m, \027[0m\027[0;33mtrue\027[0m\027[1;30m, \027[0m\027[1;32m'\027[0;32mA\027[1;32m\\n\027[0;32mstring\027[1;32m'\027[0m \027[1;30m}\027[0m")
 
-ffi.cdef[[
-typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel;
-]]
-local img = ffi.new("rgba_pixel[?]", 9)
-p{utils.stdout, img, p}
+    utils.loadColors(256)
+    local super = dump(data)
+    print("super", super, dump(super))
+    assert(super == "\027[38;5;247m{ \027[0m\027[38;5;202m42\027[0m\027[38;5;240m, \027[0m\027[38;5;220mtrue\027[0m\027[38;5;240m, \027[0m\027[38;5;40m'\027[38;5;34mA\027[38;5;46m\\n\027[38;5;34mstring\027[38;5;40m'\027[0m \027[38;5;247m}\027[0m")
 
-local tim = {name="Tim", age=32}
-local jack = {name="Jack", age=8}
-p{
-  {1,2,3,4},
-  {1,2,3,g=5,6,z=2, [10]=10},
-}
-p{
-  [tim] = "programmer",
-  [jack] = "player",
-}
-p{
-  programmer = tim,
-  player = jack,
-}
+  end)
 
-local cycle = {a="table"}
-cycle.self = cycle
-p(cycle)
+end)
+
