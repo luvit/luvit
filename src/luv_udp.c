917dca2d309e5dcc725efdcff98ca7ec61c6e815
Daniel Barney <daniel@pagodabox.com>
2014-10-31 11:16:07 -0600

    on errors, buff needs to be freed

--- src/luv_udp.c
+++ src/luv_udp.c
@@ -57,12 +57,14 @@ static void luv_on_udp_recv(uv_udp_t* handle,
   lua_State *L = luv_handle_get_lua(handle->data);
 
   if (nread == 0) {
+    free(buf.base);
     return;
   }
 
   if (nread < 0) {
     luv_push_async_error(L, uv_last_error(luv_get_loop(L)), "on_recv", NULL);
     luv_emit_event(L, "error", 1);
+    free(buf.base);
     return;
   }
 
