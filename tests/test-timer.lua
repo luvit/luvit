9a816359206bf25b174fc0bc9268f39c9ae21129
Tim Caswell <tim@creationix.com>
2014-11-03 14:56:24 -0600

    Move bind to utils, fix copyright headers

--- tests/test-timer.lua
+++ tests/test-timer.lua
@@ -1,6 +1,6 @@
 --[[
 
-Copyright 2012 The Luvit Authors. All Rights Reserved.
+Copyright 2012-2014 The Luvit Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ limitations under the License.
 --]]
 
 local timer = require('timer')
-
 
 require('tap')(function (test)
 
