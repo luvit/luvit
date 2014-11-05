34a82b12656194f4dadde0aaea6c635121fbad25
Tim Caswell <tim@creationix.com>
2014-11-05 15:49:21 -0600

    Package codec as duplex to match future tls version [ci skip]

--- app/modules/http-codec.lua
+++ app/modules/http-codec.lua
@@ -1,28 +1,31 @@
-local server = {}
-local client = {}
-exports.server = server
-exports.client = client
-
-function server.encoder(write)
-  return function (item)
-    error("TODO: implement server.encoder")
+function exports.server(app)
+  local function encoder(write)
+    return function (item)
+      error("TODO: implement server.encoder")
+    end
   end
+
+  local function decoder(emit)
+    return function (chunk)
+      error("TODO: implement server.decoder")
+    end
+  end
+
+  return decoder(app(encoder))
 end
 
-function client.encoder(write)
-  return function (item)
-    error("TODO: implement client.encoder")
+function exports.client(app)
+  local function encoder(write)
+    return function (item)
+      error("TODO: implement client.encoder")
+    end
   end
-end
 
-function client.decoder(emit)
-  return function (chunk)
-    error("TODO: implement client.decoder")
+  local function decoder(emit)
+    return function (chunk)
+      error("TODO: implement client.decoder")
+    end
   end
-end
 
-function server.decoder(emit)
-  return function (chunk)
-    error("TODO: implement server.decoder")
-  end
+  return decoder(app(encoder))
 end
--- test-http-server.lua
+++ test-http-server.lua
@@ -1,16 +1,11 @@
 local uv = require('uv')
 local httpCodec = require('http-codec').server
 
-local server = uv.new_tcp()
-uv.tcp_bind(server, "127.0.0.1", 8080)
-uv.listen(server, 128, function (_, err)
-  assert(not err, err)
-  local client = uv.new_tcp()
-  uv.accept(server, client)
-  local write = httpCodec.encoder(function (chunk)
-    uv.write(client, chunk)
-  end)
-  uv.read_start(client, httpCodec.decoder(function (req)
+-- Implement the app as a filter
+-- Input is request objects
+-- Output is response objects
+local function app(write)
+  return function (req)
     local body = "Hello Client\n"
     local res = {
       code = 200,
@@ -30,6 +25,18 @@ uv.listen(server, 128, function (_, err)
     write(body)
     -- End the stream
     write()
+  end
+end
+
+-- Hook the app to the TCP server with the httpCodec in the middle
+local server = uv.new_tcp()
+uv.tcp_bind(server, "127.0.0.1", 8080)
+uv.listen(server, 128, function (_, err)
+  assert(not err, err)
+  local client = uv.new_tcp()
+  uv.accept(server, client)
+  uv.read_start(client, httpCodec(app)(function (chunk)
+    uv.write(client, chunk)
   end))
 end)
 print("HTTP server listening at http://127.0.0.1:8080/")
