7ad200b8432c4ad2e78ec0bd0ed474baf44ab3b7
Tim Caswell <tim@creationix.com>
2014-11-13 15:44:50 -0600

    Port old tls code to new codec

--- app/modules/codecs/tls.lua
+++ app/modules/codecs/tls.lua
@@ -1,13 +1,88 @@
+--[[
+
+Copyright 2014 The Luvit Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
+local openssl = require('openssl')
+local bit = require('bit')
+
 return function (options)
-  local tls = {};
-  local outerRead, outerWrite
+
+  local ctx = openssl.ssl.ctx_new("TLSv1_2")
+  ctx:set_verify({"none"})
+  -- TODO: Make options configurable in secureChannel call
+  ctx:options(bit.bor(
+    openssl.ssl.no_sslv2,
+    openssl.ssl.no_sslv3,
+    openssl.ssl.no_compression))
+  local bin, bout = openssl.bio.mem(8192), openssl.bio.mem(8192)
+  local ssl = ctx:ssl(bin, bout, false)
+
+  local outerWrite, outerRead, waiting
+
+  -- Both sides will call handshake as they are hooked up
+  -- But the first to call handshake will simply wait
+  -- And the second will perform the handshake and then
+  -- resume the other.
+  local function handshake()
+    if outerWrite and outerRead then
+      while true do
+        if ssl:handshake() then break end
+        outerWrite(bout:read())
+        bin:write(outerRead())
+      end
+      assert(coroutine.resume(waiting))
+      waiting = nil
+    else
+      waiting = coroutine.running()
+      coroutine.yield()
+    end
+  end
+
+  local tls = {}
+
   function tls.decoder(read, write)
     outerRead = read
-    error("TODO: Implement tls decoder")
+    handshake()
+    for cipher in read do
+      bin:write(cipher)
+      if bin:pending() > 0 then
+        local data = ssl:read()
+        if data then
+          write(data)
+        end
+      end
+    end
+    write()
+    -- TODO: cleanup ssl state
   end
+
   function tls.encoder(read, write)
     outerWrite = write
-    error("TODO: Implement tls encoder")
+    handshake()
+    for plain in read do
+      ssl:write(plain)
+      if bout:pending() > 0 then
+        write(bout:read())
+      end
+    end
+    write()
+    -- TODO: cleanup ssl state
   end
+
   return tls
+
 end
---
+++ tests/test-tls-codec.lua
@@ -0,0 +1,74 @@
+--[[
+
+Copyright 2014 The Luvit Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
+local http = require('codecs/http')
+local tlsCodec = require('codecs/tls')
+local uv = require('uv')
+local chain = require('codec').chain
+local wrapStream = require('codec').wrapStream
+local x509 = require('openssl').x509
+local fs = require('fs')
+local pathJoin = require('luvi').path.join
+
+require('tap')(function (test)
+
+  test("reading ca.cer", function ()
+    local path = pathJoin(module.dir, "fixtures", "luvit.io-ca.cer")
+    local xcert = x509.read(fs.readFileSync(path))
+    p(xcert:parse())
+  end)
+
+  test("Real HTTPS request", function (expect)
+    uv.getaddrinfo("luvit.io", "https", {
+      socktype = "STREAM",
+      family = "INET",
+    }, expect(function (err, res)
+      assert(not err, err)
+      local client = uv.new_tcp()
+      uv.tcp_connect(client, res[1].addr, res[1].port, expect(function (err)
+        assert(not err, err)
+        p {
+          client = client,
+          sock = uv.tcp_getsockname(client),
+          peer = uv.tcp_getpeername(client),
+        }
+        local read, write = wrapStream(client)
+        local tls = tlsCodec()
+        chain(tls.decoder, http.client.decoder, expect(function (read, write)
+          local req = {
+            method = "GET", path = "/",
+            {"Host", "luvit.io"},
+            {"User-Agent", "luvit"},
+            {"Accept", "*/*"},
+          }
+          p(req)
+          write(req)
+          for item in read do
+            if item == "" then
+              -- Close the connection when the response body is done.
+              write()
+            else
+              p(item)
+            end
+          end
+        end), http.client.encoder, tls.encoder)(read, write)
+      end))
+    end))
+  end)
+
+end)
