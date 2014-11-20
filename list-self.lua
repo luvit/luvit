9e9c68c6045a0cfdb607569b78f53b0898a5dcdf
Tim Caswell <tim@creationix.com>
2014-11-20 13:02:19 -0600

    Save zipwriter code

--- list-self.lua
+++ list-self.lua
@@ -1,12 +1,7 @@
 local miniz = require('miniz')
 local uv = require('uv')
 local pathJoin = require('luvi').path.join
-
-local reader = miniz.new_reader(uv.exepath())
-for i = 1, reader:get_num_files() do
-  local path = reader:get_filename(i)
-  print(path)
-end
+local fs = require('fs')
 
 -- Build a new luvi* binary given a list of zip files and/or folders.
 local function build(...)
@@ -15,7 +10,8 @@ local function build(...)
   local args = {...}
 
   for i = 1, #args do
-    local path = pathJoin(cwd, args[i])
+    local path = args[i]
+    path = path == "self" and uv.exepath() or pathJoin(cwd, path)
     local stat = assert(uv.fs_stat(path))
     if stat.type == "FILE" then
       local reader = miniz.new_reader(path)
@@ -24,27 +20,63 @@ local function build(...)
         if not reader:is_directory(j) then
           local filename = reader:get_filename(j)
           print("  " .. filename)
-          files[filename] = reader:extract(j)
+          files[filename] = {reader, j}
         end
       end
       print()
     elseif stat.type == "DIRECTORY" then
-      local function importFolder(path)
-        local req = uv.fs_scandir(path)
-        local function getNext()
-          return uv.fs_scandir_next(req)
-        end
-        for path in  do
-          print(path)
+      print("Importing folder: " .. path)
+      local function importFolder(sub)
+        local fullPath = pathJoin(path, sub)
+        for name, t in fs.scandirSync(fullPath) do
+          local filename = pathJoin(sub, name)
+          if t == 'FILE' then
+            print("  " .. filename)
+            files[filename] = pathJoin(fullPath, name)
+          elseif t == 'DIR' then
+            importFolder(filename)
+          end
         end
       end
-      p(path, "FOLDER")
-      importFolder(path)
+      importFolder("")
     else
       error("Not sure how to embed " .. path)
     end
-  endgit
-  return files
+  end
+
+
+  -- Save the files to the zip in sorted order.
+  local paths = {}
+  for path in pairs(files) do
+    paths[#paths + 1] = path
+  end
+  table.sort(paths)
+  local writer = miniz.new_writer()
+  for i = 1, #paths do
+    local path = paths[i]
+    local file = files[path]
+    if type(file) == "string" then
+      p("Loading", path, file)
+      writer:add(path, fs.readFileSync(file), 9)
+    else
+      writer:add_from_zip(unpack(file))
+    end
+  end
+  return writer:finalize()
 end
 
-build(uv.exepath(), "app")
+print("Creating new binary `newapp`")
+local fd = assert(fs.openSync("newapp", "w", 511)) -- 0777
+local reader = miniz.new_reader(uv.exepath())
+local binSize = reader:get_offset()
+print("Copying initial " .. binSize .. " bytes from " .. uv.exepath())
+local fd2 = assert(fs.openSync(uv.exepath()))
+local bin = assert(fs.readSync(fd2, binSize))
+fs.closeSync(fd2)
+fs.writeSync(fd, bin)
+print("Appending zip file")
+local zip = build("self", "overlay")
+fs.writeSync(fd, zip)
+print("Closing")
+fs.closeSync(fd)
+print("Done")
