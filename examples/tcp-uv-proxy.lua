ae0372bd31344c5488c6a645461561ce7bd94049
KennethWilke <kenneth.wilke@gmail.com>
2014-12-04 15:52:37 -0600

    proposed improvements to proxy example

--- examples/tcp-uv-proxy.lua
+++ examples/tcp-uv-proxy.lua
@@ -2,10 +2,10 @@ local uv = require("uv")
 
 -- Create listening socket and bind to 127.0.0.1:8080
 local server = uv.new_tcp()
-uv.tcp_bind(server, "127.0.0.1", 8080)
+server:bind("127.0.0.1", 8080)
 
 -- Setup listener
-uv.listen(server, 128, function(error)
+server:listen(128, function(error)
   -- This function is executed for each new client
   print("New connection")
 
@@ -14,31 +14,44 @@ uv.listen(server, 128, function(error)
   local upstream = uv.new_tcp()
 
   -- Accept the client connection
-  uv.accept(server, client)
+  server:accept(client)
 
   -- Connect to upstream server
-  uv.tcp_connect(upstream, "127.0.0.1", 80, function(error)
-    -- Setup handler to send data from upstream to client
-    uv.read_start(upstream, function(err, data)
-      if err then print("Upstream error:" .. err) end
-      if data then
-        print("Upstream response: " .. data)
-        uv.write(client, data)
-      else
-        print("Upstream disconnected")
-      end
-    end)
+  upstream:connect("127.0.0.1", 80, function(error)
+    if error then
+      print('Failed to connect to upstream: ' .. error)
+      -- If can't connect to upstream, cleanup both handles
+      upstream:close()
+      client:close()
+    else
+      -- Setup handler to send data from upstream to client
+      upstream:read_start(function(err, data)
+        if err then print("Upstream error:" .. err) end
+        if data then
+          print("Upstream response: " .. data)
+          client:write(data)
+        else
+          -- Upstream disconnected, cleanup handles
+          upstream:close()
+          client:close()
+          print("Upstream disconnected")
+        end
+      end)
 
-    -- Setup handler to send data from client to upstream
-    uv.read_start(client, function(err, data)
-      if err then print("Client error:" .. err) end
-      if data then
-        print("Client request: " .. data)
-        uv.write(upstream, data)
-      else
-        print("Client disconnected")
-      end
-    end)
+      -- Setup handler to send data from client to upstream
+      client:read_start(function(err, data)
+        if err then print("Client error:" .. err) end
+        if data then
+          print("Client request: " .. data)
+          upstream:write(data)
+        else
+          -- Client disconnected, cleanup handles
+          upstream:close()
+          client:close()
+          print("Client disconnected")
+        end
+      end)
+    end
   end)
 end)
 
