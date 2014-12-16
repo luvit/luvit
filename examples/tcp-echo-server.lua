0f389235a4423ff4d1d46799d4f1d23378456eb4
KennethWilke <kenneth.wilke@gmail.com>
2014-12-16 13:58:34 -0600

    added signal handler and tcp echo server examples

---
+++ examples/tcp-echo-server.lua
@@ -0,0 +1,32 @@
+local uv = require('uv')
+
+-- Create listener socket
+local server = uv.new_tcp()
+server:bind('127.0.0.1', 1234)
+
+server:listen(128, function(err)
+  -- Create socket handle for client
+  local client = uv.new_tcp()
+
+  -- Accept incoming connection
+  server:accept(client)
+  print("Client connected")
+
+  -- Relay data back to client
+  client:read_start(function(err, data)
+    -- If error, print and close connection
+    if err then
+      print("Client read error: " .. err)
+      client:shutdown(function() client:close() end)
+    end
+
+    -- If data is set the client has sent data, if unset the client has disconnected
+    if data then
+      client:write(data)
+    else
+      print("Client disconnected")
+      client:shutdown(function() client:close() end)
+    end
+
+  end)
+end)
