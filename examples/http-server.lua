618baf9e24046010bd84f937b9b63c13473a33e4
Tim Caswell <tim@creationix.com>
2015-01-23 13:00:11 -0600

    start https server code

--- examples/http-server.lua
+++ examples/http-server.lua
@@ -1,9 +1,22 @@
 local http = require("http")
+local https = require("https")
+local pathJoin = require('luvi').path.join
+local fs = require('fs')
 
-http.createServer(function (req, res)
+local function onRequest(req, res)
+  print(req.method, req.url)
   local body = "Hello world\n"
   res:setHeader("Content-Type", "text/plain")
+  res:setHeader("Content-Length", #body)
   res:finish(body)
-end):listen(8080)
+end
 
+http.createServer(onRequest):listen(8080)
 print("Server listening at http://localhost:8080/")
+
+https.createServer({
+  key = fs.readFileSync(pathJoin(module.dir, "key.pem")),
+  cert = fs.readFileSync(pathJoin(module.dir, "cert.pem")),
+}, onRequest):listen(8443)
+print("Server listening at https://localhost:8443/")
+
