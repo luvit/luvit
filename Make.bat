0901c4997d12444983186c112bee15b7f3b3b948
Tim Caswell <tim@creationix.com>
2015-02-10 13:33:15 -0600

    Fix path in Make.bat

--- Make.bat
+++ Make.bat
@@ -11,7 +11,7 @@ GOTO :end
 ECHO "Building lit"
 git clone --depth 1 https://github.com/luvit/luvi-binaries.git
 git clone --depth 1 https://github.com/luvit/lit.git lit
-SET LUVI_APP=lit-app/app
+SET LUVI_APP=lit/app
 SET LUVI_TARGET=lit.exe
 luvi-binaries\Windows\luvi.exe
 SET "LUVI_APP="
@@ -25,5 +25,8 @@ GOTO :end
 
 :clean
 IF EXIST luvit.exe DEL /F /Q luvit.exe
+IF EXIST lit.exe DEL /F /Q lit.exe
+IF EXIST lit RMDIR /S /Q lit
+IF EXIST luvi-binaries RMDIR /S /Q luvi-binaries
 
 :end
