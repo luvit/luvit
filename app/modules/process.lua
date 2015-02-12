2d4c2efce04423d7f60c2832653b6d901f5d7ba3
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-12 14:14:42 -0600

    feat(process): kill, getpid, argv, exit

--- app/modules/process.lua
+++ app/modules/process.lua
@@ -19,11 +19,12 @@ limitations under the License.
 exports.name = "luvit/process"
 exports.version = "0.1.0"
 
-local uv = require('uv')
 local env = require('env')
+local hooks = require('hooks')
+local os = require('os')
 local timer = require('timer')
 local utils = require('utils')
-local hooks = require('hooks')
+local uv = require('uv')
 local Emitter = require('core').Emitter
 
 local function nextTick(...)
@@ -62,12 +63,49 @@ setmetatable(lenv, {
   end
 })
 
+local function kill(pid, signal)
+  uv.kill(pid, signal or 'sigterm')
+end
+
+local signalWraps = {}
+
+local function on(self, _type, listener)
+  if not signalWraps[_type] then
+    local signal = uv.new_signal()
+    signalWraps[_type] = signal
+    uv.unref(signal)
+    uv.signal_start(signal, _type, function() self:emit(_type) end)
+  end
+  Emitter.on(self, _type, listener)
+end
+
+local function removeListener(self, _type, listener)
+  local signal = signalWraps[_type]
+  if not signal then return end
+  signal:stop()
+  uv.close(signal)
+  signalWraps[_type] = nil
+  Emitter.removeListener(self, _type, listener)
+end
+
+local function exit(self, code)
+  code = code or 0
+  self:emit('exit', code)
+  os.exit(code)
+end
+
 local function globalProcess()
   local process = Emitter:new()
+  process.argv = args
   process.exitCode = 0
   process.nextTick = nextTick
   process.env = lenv
   process.cwd = cwd
+  process.kill = kill
+  process.pid = uv.getpid()
+  process.on = on
+  process.exit = exit
+  process.removeListener = removeListener
   hooks:on('process.exit', utils.bind(process.emit, process, 'exit'))
   return process
 end
