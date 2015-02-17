61cf365844bc05813e901bdc82bf360d1ed028a4
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-17 15:34:31 -0600

    remove to-convert/test-env... migrated to misc test

--- tests/to-convert/test-env.lua
+++
@@ -1,8 +0,0 @@
-local env = require('env')
-
-local testv_name = 'TEST_VARIABLE'
-local testv_value = 'Test Value'
-
-
-env.set(testv_name, testv_value, 1)
-assert(testv_value == env.get(testv_name))
