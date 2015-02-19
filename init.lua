6b06e3a2f030896f5c411190f2c9f3da21c21aa5
Tim Caswell <tim@creationix.com>
2015-02-19 08:38:34 -0600

    Restructure luvit's init to be easier to consume

--- init.lua
+++ init.lua
@@ -18,15 +18,16 @@ limitations under the License.
 local utils = require('utils')
 local uv = require('uv')
 
-local function init()
+
+return function (main, ...)
   -- Make print go through libuv for windows colors
   _G.print = utils.print
   -- Register global 'p' for easy pretty printing
   _G.p = utils.prettyPrint
   _G.process = require('process').globalProcess()
-end
 
-local function run()
+  -- Call the main app
+  main(...)
 
   -- Start the event loop
   uv.run()
@@ -36,7 +37,6 @@ local function run()
   -- When the loop exits, close all uv handles.
   uv.walk(uv.close)
   uv.run()
-end
 
-exports.init = init
-exports.run = run
+  return _G.process.exitCode
+end
--- main.lua
+++ main.lua
@@ -16,137 +16,132 @@ limitations under the License.
 
 --]]
 
+-- Bootstrap the require system
 local luvi = require('luvi')
-local bundle = luvi.bundle
-
--- Manually register the require replacement system to bootstrap things
-bundle.register("require", "modules/require.lua");
--- Upgrade require system in-place
+luvi.bundle.register('require', "modules/require.lua")
 local require = require('require')()("bundle:main.lua")
 
-local luvit = require('./init')
-luvit.init()
+-- Create a luvit powered main that does the luvit CLI interface
+require('./init')(function (...)
 
-local uv = require('uv')
-local utils = require('utils')
+  local uv = require('uv')
+  local utils = require('utils')
 
-local startRepl = nil
-local combo = nil
-local script = nil
-local extra = {}
+  local startRepl = nil
+  local combo = nil
+  local script = nil
+  local extra = {}
 
-local function usage()
-  print("Usage: " .. args[0] .. " [options] script.lua [arguments]"..[[
-
-
-Options:
-  -h, --help          Print this help screen.
-  -v, --version       Print the version.
-  -e code_chunk       Evaluate code chunk and print result.
-  -i, --interactive   Enter interactive repl after executing script.
-  -n, --no-color      Disable colors.
-  -c, --16-colors     Use simple ANSI colors
-  -C, --256-colors    Use 256-mode ANSI colors
-                      (Note, if no script is provided, a repl is run instead.)
-]])
-  startRepl = false
-end
-
-local function version()
-  print("TODO: show luvit version")
-  startRepl = false
-end
+  local function usage()
+    print("Usage: " .. args[0] .. " [options] script.lua [arguments]"..[[
 
 
-local shorts = {
-  h = "help",
-  v = "version",
-  e = "eval",
-  i = "interactive",
-  n = "no-color",
-  c = "16-colors",
-  C = "256-colors",
-}
-
-local flags = {
-  help = usage,
-  version = version,
-  eval = function ()
-    local repl = require('repl')(utils.stdin, utils.stdout)
-    combo = repl.evaluateLine
+  Options:
+    -h, --help          Print this help screen.
+    -v, --version       Print the version.
+    -e code_chunk       Evaluate code chunk and print result.
+    -i, --interactive   Enter interactive repl after executing script.
+    -n, --no-color      Disable colors.
+    -c, --16-colors     Use simple ANSI colors
+    -C, --256-colors    Use 256-mode ANSI colors
+                        (Note, if no script is provided, a repl is run instead.)
+  ]])
     startRepl = false
-  end,
-  interactive = function ()
-    startRepl = true
-  end,
-  ["no-color"] = function ()
-    utils.loadColors()
-  end,
-  ["16-colors"] = function ()
-    utils.loadColors(16)
-  end,
-  ["256-colors"] = function ()
-    utils.loadColors(256)
-  end,
-}
+  end
 
-for i = 1, #args do
-  local arg = args[i]
-  if script then
-    extra[#extra + 1] = arg
-  elseif combo then
-    combo(arg)
-    combo = nil
-  elseif string.sub(arg, 1, 1) == "-" then
-    local flag
-    if (string.sub(arg, 2, 2) == "-") then
-      flag = string.sub(arg, 3)
+  local function version()
+    print("TODO: show luvit version")
+    startRepl = false
+  end
+
+
+  local shorts = {
+    h = "help",
+    v = "version",
+    e = "eval",
+    i = "interactive",
+    n = "no-color",
+    c = "16-colors",
+    C = "256-colors",
+  }
+
+  local flags = {
+    help = usage,
+    version = version,
+    eval = function ()
+      local repl = require('repl')(utils.stdin, utils.stdout)
+      combo = repl.evaluateLine
+      startRepl = false
+    end,
+    interactive = function ()
+      startRepl = true
+    end,
+    ["no-color"] = function ()
+      utils.loadColors()
+    end,
+    ["16-colors"] = function ()
+      utils.loadColors(16)
+    end,
+    ["256-colors"] = function ()
+      utils.loadColors(256)
+    end,
+  }
+
+  for i = 1, #args do
+    local arg = args[i]
+    if script then
+      extra[#extra + 1] = arg
+    elseif combo then
+      combo(arg)
+      combo = nil
+    elseif string.sub(arg, 1, 1) == "-" then
+      local flag
+      if (string.sub(arg, 2, 2) == "-") then
+        flag = string.sub(arg, 3)
+      else
+        arg = string.sub(arg, 2)
+        flag = shorts[arg] or arg
+      end
+      local fn = flags[flag] or usage
+      fn()
     else
-      arg = string.sub(arg, 2)
-      flag = shorts[arg] or arg
+      script = arg
     end
-    local fn = flags[flag] or usage
-    fn()
-  else
-    script = arg
   end
-end
 
-if combo then error("Missing flag value") end
+  if combo then error("Missing flag value") end
 
-if startRepl == nil and not script then startRepl = true end
+  if startRepl == nil and not script then startRepl = true end
 
--- Seed Lua's RNG
-do
-  local math = require('math')
-  local os = require('os')
-  math.randomseed(os.clock())
-end
-
-if script then
-  require(luvi.path.join(uv.cwd(), script))
-end
-
-if startRepl then
-  local env = require('env')
-  local pathJoin = require('luvi').path.join
-  local fs = require('fs')
-  local c = utils.color
-  local greeting = "Welcome to the " .. c("err") .. "L" .. c("quotes") .. "uv" .. c("table") .. "it" .. c() .. " repl!"
-  local historyFile
-  if require('ffi').os == "Windows" then
-    historyFile = pathJoin(env.get("APPDATA"), "luvit_history")
-  else
-    historyFile = pathJoin(env.get("HOME"), ".luvit_history")
+  -- Seed Lua's RNG
+  do
+    local math = require('math')
+    local os = require('os')
+    math.randomseed(os.clock())
   end
-  local lines = fs.readFileSync(historyFile) or ""
-  local function saveHistory(lines)
-    fs.writeFileSync(historyFile, lines)
+
+  if script then
+    require(luvi.path.join(uv.cwd(), script))
   end
-  require('repl')(utils.stdin, utils.stdout, greeting, ...).start(lines, saveHistory)
 
-end
+  if startRepl then
+    local env = require('env')
+    local pathJoin = require('luvi').path.join
+    local fs = require('fs')
+    local c = utils.color
+    local greeting = "Welcome to the " .. c("err") .. "L" .. c("quotes") .. "uv" .. c("table") .. "it" .. c() .. " repl!"
+    local historyFile
+    if require('ffi').os == "Windows" then
+      historyFile = pathJoin(env.get("APPDATA"), "luvit_history")
+    else
+      historyFile = pathJoin(env.get("HOME"), ".luvit_history")
+    end
+    local lines = fs.readFileSync(historyFile) or ""
+    local function saveHistory(lines)
+      fs.writeFileSync(historyFile, lines)
+    end
+    require('repl')(utils.stdin, utils.stdout, greeting, ...).start(lines, saveHistory)
 
-luvit.run()
+  end
+end, ...)
 
-return _G.process.exitCode
