1a13be87642c0034c196d228d03a038369b6303e
Tim Caswell <tim@creationix.com>
2015-02-23 14:34:16 -0600

    Rename Make.bat to be lowercase

--- make.bat
+++ make.bat
@@ -3,7 +3,7 @@
 IF NOT "x%1" == "x" GOTO :%1
 
 :luvit
-IF NOT EXIST lit.exe CALL Make.bat lit
+IF NOT EXIST lit.exe CALL make.bat lit
 ECHO "Building luvit"
 lit.exe make
 GOTO :end
@@ -11,11 +11,10 @@ GOTO :end
 :lit
 ECHO "Building lit"
 PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/luvit/lit/raw/0.9.8/web-install.ps1'))"
-IF EXIST lit.zip DEL /F /Q lit.zip
 GOTO :end
 
 :test
-IF NOT EXIST luvit.exe CALL Make.bat luvit
+IF NOT EXIST luvit.exe CALL make.bat luvit
 ECHO "Testing luvit"
 SET LUVI_APP=.
 luvit.exe tests\run.lua
