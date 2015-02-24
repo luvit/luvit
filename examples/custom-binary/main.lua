9c8d382f1d6a83e5f448916ad7ee833046e80429
Tim Caswell <tim@creationix.com>
2015-02-24 10:51:19 -0600

    Update custom-binary example to use lit and latest luvit API

---
+++ examples/custom-binary/main.lua
@@ -0,0 +1,13 @@
+-- Bootstrap the require system
+local luvi = require('luvi')
+luvi.bundle.register('require', "modules/require.lua")
+local require = require('require')()("bundle:main.lua")
+
+-- Create a luvit powered main that does the luvit CLI interface
+return require('luvit')(function (...)
+  print("Custom main with custom builtins!")
+  print("1 + 2", require("add")(1, 2))
+  print("3 - 2", require("multi")(3, 2))
+  p(process.argv)
+  p(...)
+end, ...)
