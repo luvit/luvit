1d9f29e911f9d57891823603bec1b29919ea88ea
Tim Caswell <tim@creationix.com>
2015-03-04 13:26:11 -0600

    Use Transform in stream example for Joystick

--- examples/linux-joystick-stream.lua
+++ examples/linux-joystick-stream.lua
@@ -1,8 +1,16 @@
 local fs = require('fs')
 local Buffer = require('buffer').Buffer
+local Transform = require('stream').Transform
+
+local JoystickTransform = Transform:extend()
+
+function JoystickTransform:initialize()
+  Transform.initialize(self, {objectMode = true})
+end
 
 -- https://www.kernel.org/doc/Documentation/input/joystick-api.txt
-local function parse(buffer)
+function JoystickTransform:_transform(chunk, _, callback)
+  local buffer = Buffer:new(chunk)
   local event = {
     time   = buffer:readUInt32LE(1),
     number = buffer:readUInt8(8),
@@ -12,15 +20,17 @@ local function parse(buffer)
   if bit.band(type, 0x80) > 0 then event.init = true end
   if bit.band(type, 0x01) > 0 then event.type = "button" end
   if bit.band(type, 0x02) > 0 then event.type = "axis" end
-  return event
+  self:push(event)
+  callback()
 end
 
-local readStream = fs.createReadStream("/dev/input/js0", { chunkSize = 8 })
 
-readStream:on("data", function (chunk)
-  p(parse(Buffer:new(chunk)))
-end)
+local input = fs.createReadStream("/dev/input/js0", { chunkSize = 8 })
+local events = JoystickTransform:new()
+input:pipe(events)
 
-readStream:on("error", error)
+events:on("data", p)
+input:on("error", error)
+events:on("error", error)
 
 coroutine.yield()
