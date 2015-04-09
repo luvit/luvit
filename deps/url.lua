246d6a9caf4e8d5e8e510cf9bfb0820851186ce8
gavellaneda <avellaneda.gabriel@gmail.com>
2015-04-09 14:44:15 -0300

    Minor changes

--- deps/url.lua
+++ deps/url.lua
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 --]]
-local exports = {}
 exports.name = "luvit/url"
 exports.version = "1.0.0"
 
