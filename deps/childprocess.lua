c402b7c3da00506d125dfaeb74a83ccc829fae01
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-13 17:38:15 -0500

    fix(childprocess): propagate errors through the pipes

--- deps/childprocess.lua
+++ deps/childprocess.lua
@@ -23,7 +23,6 @@ local core = require('core')
 local net = require('net')
 local timer = require('timer')
 local uv = require('uv')
-local utils = require('utils')
 
 local Error = core.Error
 
@@ -44,26 +43,30 @@ end
 
 function Process:kill(signal)
   if self.handle then uv.process_kill(self.handle, signal or 'sigterm') end
-  self:close()
+  self:close(Error:new('killed'))
 end
 
-function Process:close()
+function Process:close(err)
   if self.handle then uv.close(self.handle) end
-  self:destroy()
+  self:destroy(err)
 end
 
 function Process:destroy(err)
-  self:_cleanup()
+  self:_cleanup(err)
   if err then
-    timer.setImmediate(utils.bind(self.emit, self, 'error', err))
+    timer.setImmediate(function() self:emit('error', err) end)
   end
 end
 
-function Process:_cleanup()
-  self.stdout:on('end', function() self.stdout:destroy() end)
-  self.stdout:resume()
-  self.stderr:destroy()
-  self.stdin:destroy()
+function Process:_cleanup(err)
+  if err then
+    self.stdout:destroy(err)
+  else
+    self.stdout:on('end', function() self.stdout:destroy() end)
+    self.stdout:resume()
+  end
+  self.stderr:destroy(err)
+  self.stdin:destroy(err)
 end
 
 local function spawn(command, args, options)
@@ -109,8 +112,10 @@ local function spawn(command, args, options)
   em:setPid(pid)
 
   if em.handle == nil then
-    process.nextTick(utils.bind(em.emit, em, 'exit', -127))
-    em:destroy(Error:new(pid))
+    timer.setImmediate(function()
+      em:emit('exit', -127)
+      em:destroy(Error:new(pid))
+    end)
   end
 
   return em
