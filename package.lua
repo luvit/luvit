345f1119050593b7b0b9ada5c5329e375b2edf02
Tim Caswell <tim@creationix.com>
2015-05-08 14:36:36 -0500

    Add license to luvit package

--- package.lua
+++ package.lua
@@ -1,6 +1,20 @@
 return {
   name = "luvit/luvit",
-  version = "2.1.5",
+  version = "2.1.5-1",
+  license = "Apache 2",
+  homepage = "https://github.com/luvit/luvit",
+  description = "node.js style APIs for luvi as either a luvi app or library.",
+  tags = { "luvit", "meta" },
+  author = { name = "Tim Caswell" },
+  contributors = {
+    "Ryan Phillips",
+    "George Zhao",
+    "Rob Emanuele",
+    "Daniel Barney",
+    "Ryan Liptak",
+    "Kenneth Wilke",
+    "Gabriel Nicolas Avellaneda",
+  },
   luvi = {
     version = "2.0.4",
     flavor = "regular",
@@ -40,21 +54,7 @@ return {
     "!tests",
     "!bench",
     "!lit-*",
-  },
-  homepage = "https://github.com/luvit/luvit",
-  description = "node.js style APIs for luvi as either a luvi app or library.",
-  tags = { "luvit", "meta" },
-  author = { name = "Tim Caswell" },
-  contributors = {
-    "Ryan Phillips",
-    "George Zhao",
-    "Rob Emanuele",
-    "Daniel Barney",
-    "Ryan Liptak",
-    "Kenneth Wilke",
-    "Gabriel Nicolas Avellaneda",
   }
-
 }
 
 
