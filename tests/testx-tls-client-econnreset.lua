e0ec7c8b9b121b2c1d53f7739e4138f44e8b0d6a
zhaozg <zhaozg@gmail.com>
2015-05-10 11:45:21 +0800

    converted test-tls-client-econnreset.lua

---
+++ tests/testx-tls-client-econnreset.lua
@@ -0,0 +1,62 @@
+require('tap')(function (test)
+  local fixture = require('./fixture-tls')
+  local childprocess = require('childprocess')
+  local os = require('os')
+  local los = require('los')
+  local tls = require('tls')
+  local timer = require('timer')
+
+  local args = {
+    's_server',
+    '-accept', fixture.commonPort,
+    '-key', 'tests/fixtures/keys/agent1-key.pem',
+    '-cert', 'tests/fixtures/keys/agent1-cert.pem',
+  }
+  
+  test("tls client econnreset", function()
+    if(los.type() == 'win32') then return end
+    local child = childprocess.spawn('openssl', args)
+    child:on('error', function(err)
+      p(err)
+    end)
+    child:on('exit', function(exit_status)
+      print('server exited')
+    end)
+    child:on('error', function(err)
+      p(err)
+    end)
+    child.stdout:on('data', function(data)
+      print('server: ' .. data)
+    end)
+    child.stderr:on('data', function(data)
+      print('server: ' .. data)
+    end)
+
+    interval = timer.setInterval(100, function()
+      local success, err = pcall(child.stdin.write, child.stdin, "Hello world")
+    end)
+
+    timer.setTimeout(200,function ()
+      local c
+      c = tls.connect({port = fixture.commonPort, host = '127.0.0.1'})
+      c:on('error', function(err)
+        print("got connection error")
+        p(err)
+        c:destroy()
+      end)
+      c:on('close', function()
+        print('got close signal')
+      end)
+      c:on('data', function(data)
+        print('client got: ' .. data)
+      end)
+      c:on('end', function()
+        c:destroy()
+      end)
+    end)
+
+    timer.setTimeout(1000, function()
+      child:kill(9)
+    end)
+  end)
+end)
