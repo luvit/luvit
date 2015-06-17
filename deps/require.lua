76ae7b9da013aff92a38fd716a42607cf44fe9bc
Tim Caswell <tim@creationix.com>
2015-06-17 12:43:58 -0500

    require@1.2.1: Allow absolute bundle paths in require

--- deps/require.lua
+++ deps/require.lua
@@ -18,7 +18,7 @@ limitations under the License.
 
 if exports then
   exports.name = "luvit/require"
-  exports.version = "1.2.0-1"
+  exports.version = "1.2.1"
   exports.homepage = "https://github.com/luvit/luvit/blob/master/deps/require.lua"
   exports.description = "Luvit's custom require system with relative requires and sane search paths."
   exports.tags = {"luvit", "require"}
