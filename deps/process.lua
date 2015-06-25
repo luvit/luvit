55aa90da353187c7f905525c4c244e2506d1ab39
Ryan Phillips <ryan.phillips@rackspace.com>
2015-06-25 10:00:29 -0500

    bump versions

--- deps/process.lua
+++ deps/process.lua
@@ -17,7 +17,7 @@ limitations under the License.
 --]]
 
 exports.name = "luvit/process"
-exports.version = "1.1.0-3"
+exports.version = "1.1.1-3"
 exports.dependencies = {
   "luvit/hooks@1.0.0",
   "luvit/timer@1.0.0",
