2a2580dda4dadd12a6ae501999e72b48f87fc5ba
Tim Caswell <tim@creationix.com>
2015-08-04 13:39:01 -0500

    querystring: Bump version to 1.0.2

--- deps/querystring.lua
+++ deps/querystring.lua
@@ -17,7 +17,7 @@ limitations under the License.
 --]]
 
 exports.name = "luvit/querystring"
-exports.version = "1.0.1"
+exports.version = "1.0.2"
 exports.license = "Apache 2"
 exports.homepage = "https://github.com/luvit/luvit/blob/master/deps/querystring.lua"
 exports.description = "Node-style query-string codec for luvit"
