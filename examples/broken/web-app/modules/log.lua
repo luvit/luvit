02d4b085379fa2d7507124db0a9b4921bae1fa32
zhaozg <zhaozg@gmail.com>
2015-08-29 16:04:04 +0800

    delete examples/broken/web-app show setup web server, because it already as a part of httpd standard lib which more simple and flexible.

--- examples/broken/web-app/modules/log.lua
+++
@@ -1,8 +0,0 @@
-return function (app)
-  return function (req, res)
-    app(req, function (code, headers, body)
-      print(req.method .. ' ' .. req.url.path .. ' ' .. code)
-      res(code, headers, body)
-    end)
-  end
-end
