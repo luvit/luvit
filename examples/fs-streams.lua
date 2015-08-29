e498abf9b3b8179ab09e9c63b5129ce4cc334206
zhaozg <zhaozg@gmail.com>
2015-08-29 14:37:48 +0800

    converted examples/fs-streams.lua

--- examples/fs-streams.lua
+++ examples/fs-streams.lua
@@ -1,11 +1,14 @@
 local fs = require('fs')
 
 print("Reading file as stream")
-local stream = fs.createReadStream(__dirname .. "/../README.markdown")
+local path = module.dir .. "/../README.markdown"
+p('path:',path)
+local stream = fs.createReadStream(path)
 
 print("Adding data listener")
-stream:on('data', function (chunk, len)
-  p("on_data", chunk, len)
+stream:on('data', function (chunk)
+  p("on_data")
+  print(chunk)
 end)
 
 print("Adding end listener")
