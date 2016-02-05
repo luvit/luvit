7b951e5ebd2f5eabd831897bf0b4bed5427001b9
Ryan Phillips <ryan.phillips@rackspace.com>
2016-02-05 10:52:05 -0600

    slight tweak to build on unix again

--- deps/dns.lua
+++ deps/dns.lua
@@ -56,48 +56,51 @@ local rshift = bit.rshift
 local lshift = bit.lshift
 local insert = table.insert
 local concat = table.concat
+local ipapi 
 
-ffi.cdef[[
-  typedef uint32_t DWORD; //Integer
-  typedef uint32_t ULONG; //Alias
-  typedef uint32_t UINT; //Alias
-  typedef ULONG *PULONG; //Pointer
-
-  enum {
-    ERROR_SUCCESS         = 0L,
-    ERROR_BUFFER_OVERFLOW = 111L,
-    MAX_HOSTNAME_LEN      = 128,
-    MAX_DOMAIN_NAME_LEN   = 128,
-    MAX_SCOPE_ID_LEN      = 256
-  };
-
-  typedef struct {
-    char String[16];
-  } IP_ADDRESS_STRING, *PIP_ADDRESS_STRING, IP_MASK_STRING, *PIP_MASK_STRING;
-
-  typedef struct _IP_ADDR_STRING {
-    struct _IP_ADDR_STRING* Next;
-    IP_ADDRESS_STRING IpAddress;
-    IP_MASK_STRING IpMask;
-    DWORD Context;
-  } IP_ADDR_STRING, *PIP_ADDR_STRING;
-
-  typedef struct {
-    char HostName[ MAX_HOSTNAME_LEN + 4 ];
-    char DomainName[ MAX_DOMAIN_NAME_LEN + 4 ];
-    PIP_ADDR_STRING CurrentDnsServer;
-    IP_ADDR_STRING DnsServerList;
-    UINT NodeType;
-    char ScopeId[ MAX_SCOPE_ID_LEN + 4 ];
-    UINT EnableRouting;
-    UINT EnableProxy;
-    UINT EnableDns;
-  } FIXED_INFO, *PFIXED_INFO;
-
-  DWORD GetNetworkParams(PFIXED_INFO pFixedInfo, PULONG pOutBufLen);
-]]
-
-local ipapi = ffi.load('Iphlpapi.dll')
+if ffi.os=='Windows' then
+  ffi.cdef[[
+    typedef uint32_t DWORD; //Integer
+    typedef uint32_t ULONG; //Alias
+    typedef uint32_t UINT; //Alias
+    typedef ULONG *PULONG; //Pointer
+  
+    enum {
+      ERROR_SUCCESS         = 0L,
+      ERROR_BUFFER_OVERFLOW = 111L,
+      MAX_HOSTNAME_LEN      = 128,
+      MAX_DOMAIN_NAME_LEN   = 128,
+      MAX_SCOPE_ID_LEN      = 256
+    };
+  
+    typedef struct {
+      char String[16];
+    } IP_ADDRESS_STRING, *PIP_ADDRESS_STRING, IP_MASK_STRING, *PIP_MASK_STRING;
+  
+    typedef struct _IP_ADDR_STRING {
+      struct _IP_ADDR_STRING* Next;
+      IP_ADDRESS_STRING IpAddress;
+      IP_MASK_STRING IpMask;
+      DWORD Context;
+    } IP_ADDR_STRING, *PIP_ADDR_STRING;
+  
+    typedef struct {
+      char HostName[ MAX_HOSTNAME_LEN + 4 ];
+      char DomainName[ MAX_DOMAIN_NAME_LEN + 4 ];
+      PIP_ADDR_STRING CurrentDnsServer;
+      IP_ADDR_STRING DnsServerList;
+      UINT NodeType;
+      char ScopeId[ MAX_SCOPE_ID_LEN + 4 ];
+      UINT EnableRouting;
+      UINT EnableProxy;
+      UINT EnableDns;
+    } FIXED_INFO, *PFIXED_INFO;
+  
+    DWORD GetNetworkParams(PFIXED_INFO pFixedInfo, PULONG pOutBufLen);
+  ]]
+  
+  ipapi = ffi.load('Iphlpapi.dll')
+end
 
 local DEFAULT_SERVERS = {
   {
