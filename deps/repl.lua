8a93da782d5566a4dcf32ce9e1926cb75bfbfc80
Tim Caswell <tim@creationix.com>
2016-12-16 12:26:12 -0600

    Release repl 2.1.0

--- deps/repl.lua
+++ deps/repl.lua
@@ -18,7 +18,7 @@ limitations under the License.
 
 --[[lit-meta
   name = "luvit/repl"
-  version = "2.0.2"
+  version = "2.1.0"
   dependencies = {
     "luvit/utils@2.0.0",
     "luvit/readline@2.0.0",
