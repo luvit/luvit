d63bd24ce3293afe79d77e2b09baf186e92efd69
SinisterRectus <sinister.rectus@gmail.com>
2017-06-11 13:31:35 -0400

    Levenshtein optimizations

--- deps/helpful.lua
+++ deps/helpful.lua
@@ -26,34 +26,33 @@ limitations under the License.
 ]]
 
 function string.levenshtein(str1, str2)
-  local len1 = string.len(str1)
-  local len2 = string.len(str2)
-  local matrix = {}
-  local cost
-  -- quick cut-offs to save time
-  if (len1 == 0) then
-    return len2
-  elseif (len2 == 0) then
-    return len1
-  elseif (str1 == str2) then
+
+  -- cost is 0 for equal strings
+  if str1 == str2 then
     return 0
   end
-    -- initialise the base matrix values
-  for i = 0, len1, 1 do
-    matrix[i] = {}
-    matrix[i][0] = i
+
+  local len1 = string.len(str1)
+  local len2 = string.len(str2)
+
+  -- quick cut-offs to save time
+  if len1 == 0 then
+    return len2
+  elseif len2 == 0 then
+    return len1
   end
-  for j = 0, len2, 1 do
+    -- initialise the base matrix values
+  local matrix = {}
+  for i = 0, len1 do
+    matrix[i] = {[0] = i}
+  end
+  for j = 0, len2 do
     matrix[0][j] = j
   end
     -- actual Levenshtein algorithm
-  for i = 1, len1, 1 do
-    for j = 1, len2, 1 do
-      if (str1:byte(i) == str2:byte(j)) then
-        cost = 0
-      else
-        cost = 1
-      end
+  for i = 1, len1 do
+    for j = 1, len2 do
+      local cost = str1:byte(i) == str2:byte(j) and 0 or 1
       matrix[i][j] = math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost)
     end
   end
