3d01d9aa21f3bd48360c14a781a2ae16f6f70491
zhaozg <zhaozg@gmail.com>
2019-05-16 23:09:45 +0800

    clean broken examples
    * mv examples/broken/speed-http.lua to examples/speed-http.lua, and update
    * remove files in examples/broken

--- examples/broken/fs-watchfile.lua
+++
@@ -1,8 +0,0 @@
-local Watcher = require('uv').Watcher
-
-local watcher = Watcher:new('.')
-watcher:on("change", function (event, path)
-  p("on_change", {event=event,path=path})
-end);
-
-p(watcher)
