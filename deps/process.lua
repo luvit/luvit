c5535717e3a8081dbbd39a57c6845a9b04b0d863
Ryan Liptak <squeek502@hotmail.com>
2020-02-05 02:17:30 -0800

    Fix process.stdin abort when stdin is redirected from a file
    This is a hacky band-aid fix that should be replaced with a more comprehensive solution, see https://github.com/luvit/luvit/issues/1094
    
    This only 'fixes' process.stdin, but utils.stdin and pretty-print.stdin will still abort if they are attempted to be read from when stdin is a file.
    
    This does fix the example code in https://github.com/luvit/luvit/issues/1023, though

--- deps/process.lua
+++ deps/process.lua
@@ -210,7 +210,14 @@ local function globalProcess()
   process.memoryUsage = memoryUsage
   process.cpuUsage = cpuUsage
   process.removeListener = removeListener
-  process.stdin = UvStreamReadable:new(pp.stdin)
+  if uv.guess_handle(0) ~= "file" then
+    process.stdin = UvStreamReadable:new(pp.stdin)
+  else
+    -- special case for 'file' stdin handle to avoid aborting from
+    -- reading from a pipe to a file descriptor
+    -- see https://github.com/luvit/luvit/issues/1094
+    process.stdin = require('fs').ReadStream:new(nil, {fd=0})
+  end
   process.stdout = UvStreamWritable:new(pp.stdout)
   process.stderr = UvStreamWritable:new(pp.stderr)
   hooks:on('process.exit', utils.bind(process.emit, process, 'exit'))
