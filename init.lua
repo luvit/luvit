c756dada7b65e9c29380ec46314a5029d955617d
RemRBLX <114203077+RemRBLX@users.noreply.github.com>
2022-09-23 00:25:02 -0400

    add string.split

--- init.lua
+++ init.lua
@@ -44,6 +44,28 @@ return function (main, ...)
   end
 
   local args = {...}
+    -- discordia <3
+  string.split = function(str, delim)
+	local ret = {}
+	if not str then
+		return ret
+	end
+	if not delim or delim == '' then
+		for c in string.gmatch(str, '.') do
+			insert(ret, c)
+		end
+		return ret
+	end
+	local n = 1
+	while true do
+		local i, j = string.find(str, delim, n)
+		if not i then break end
+		table.insert(ret, string.sub(str, n, i - 1))
+		n = j + 1
+	end
+	table.insert(ret, string.sub(str, n))
+	return ret
+end
   local success, err = xpcall(function ()
     -- Call the main app inside a coroutine
     local utils = require('utils')
